a90f29cf643f225cc99c0c9c39571d9d1efa60e0
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-08-25 13:45:18 +0800

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   get 'likes/create'
   resources :posts, only: [:create, :new] do
+    resources :comments, only: [:create, :new]
     resources :likes, only: :create do
       collection do
         delete :destroy
       end
     end
   end
-  get 'site/index'
   devise_for :users
   root "site#index"
 end
