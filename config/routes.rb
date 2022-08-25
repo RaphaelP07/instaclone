285ec9cd7ad989a1c4ee845c1a0509e204f3c630
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-08-25 11:22:47 +0800

    adding unlike

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'likes/create'
   resources :posts, only: [:create, :new] do
-    resources :likes, only: :create
+    resources :likes, only: :create do
+      collection do
+        delete :destroy
+      end
+    end
   end
   get 'site/index'
   devise_for :users
